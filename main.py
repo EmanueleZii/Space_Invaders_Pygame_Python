cbecc0a75819a99e99803b76b98569e4e1c74c4a
Emanuele Zignale <zignale92@gmail.com>
2025-11-01 13:26:19 +0100

    +enemy sprite, +summary comment, bugfix Wall axes Y solved

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+'''
+<summary>
+   Classic VideoGame Space Invaders
+   Use:
+     - Python3
+     - Pygame
+</summary>
+'''
+
 import pygame
 
 pygame.init()
@@ -16,11 +25,17 @@ pygame.display.set_icon(icon)
 
 #Player
 playerimage = pygame.image.load('asset/Ship_1.png')
-
-#variabili globali
+# Variabili Globali Player
 speed = 0.1
 playerX = 300
 playerY = 300
+
+# Enemy
+enemyimage = pygame.image.load('asset/Ship_3.png')
+# Variabili Globali Enemy
+speed = 0.1
+enemyX = 300
+enemyY = 48
 
 #UI
 font = pygame.font.Font('freesansbold.ttf', 10)
@@ -34,11 +49,15 @@ def draw_text(text, x, y, color=(255, 255, 255)):
 def player(x,y):
     screen.blit(playerimage,(x, y))
 
+def Enemy(x,y):
+    screen.blit(enemyimage, (x, y))
+
 #update the screen
 def update():
     # rgb display
     screen.fill((0, 0, 20))
     player(playerX, playerY)
+    Enemy(enemyX, enemyY)
     Control()
     draw_text(f"Posizione: {playerX}, {playerY}", 10, 10)
     pygame.display.update()
@@ -70,15 +89,29 @@ def Control():
     if playerX >= 580:
         playerX = 580
 
-    print(f"posX: {playerX}, posY: {playerY}")
-
-#game loop
+#Game Loop
 running = True
 while running:
     update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
